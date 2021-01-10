28a48f2a5f7dbe809273a57e271eec135600f4ed
Daniel <danielnottingham.ti@gmail.com>
2021-01-10 12:11:25 -0300

    Test model AdminUser, config factory and methods

--- spec/factories/admin_users.rb
+++ spec/factories/admin_users.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :admin_user do
-    
+    email { Faker::Internet.email }
+    password { Faker::Number.number(digits: 6)}
+    encrypted_password { Faker::Number.number(digits: 6) }
   end
 end
--- spec/models/admin_user_spec.rb
+++ spec/models/admin_user_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe AdminUser, type: :model do
-  
+  describe 'When data is valid' do
+    let(:admin_user) { create(:admin_user) }
+
+    it 'is valid' do
+      expect(admin_user).to be_valid
+    end
+  end
+
+  describe 'When email is not valid' do
+    let(:admin_user) { AdminUser.new(email: 'daniel@gmail@.com', password: 123) }
+
+    it 'is not valid' do
+      expect(admin_user).to be_invalid
+    end
+  end
 end
